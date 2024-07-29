83d3c61f3c279f7caf7ee7a2bacc4b4c8991592c
Jacob Smith <jts2052@gmail.com>
2024-07-29 15:47:29 -0400

    init dev, added date and time settings and

--- src/extension.ts
+++ src/extension.ts
@@ -1,185 +1,297 @@
-import * as vscode from "vscode";
-import { Task, SubTask } from "./types/tasks";
-
-const taskDecorationType = vscode.window.createTextEditorDecorationType({
-  after: {
-    margin: "0 0 0 0.25em",
-    color: "gray",
-  },
-});
-
-// TODO: Change this, get data from config file
-const boldAndLargeDecorationType = vscode.window.createTextEditorDecorationType(
-  {
-    rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
-    textDecoration: "font-weight: bold; font-size: 1.2em",
-  }
-);
+import * as vscode from 'vscode'
+import { Task, SubTask, CompletionStatus } from './types/tasks'
 
 export function activate(context: vscode.ExtensionContext) {
-  let tasks: { [key: number]: Task } = {};
+	const config = vscode.workspace.getConfiguration('smarTODO')
+	const hideCompletedTasks = config.get<boolean>('hideCompletedTasks')
+	const completionHighlightsEnabled = config.get<boolean>(
+		'enableCompletionHighlights'
+	)
+	const completionHighlightsColor = config.get<string>(
+		'completedHighlightColor'
+	)
+	const incompleteHighlightsColor = config.get<string>(
+		'incompleteHighlightColor'
+	)
+	const showCompletionDate = config.get<boolean>('showCompletionDate')
+	const showCreationDate = config.get<boolean>('showCreationDate')
+	const showCreationTime = config.get<boolean>('showCreationTime')
 
-  let findAndUpdateTODOs = () => {
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      return;
-    }
-    const document = editor.document;
-    const text = document.getText();
-    const taskRegex: RegExp = /{([^}]+)}/g;
-    const subTaskRegex: RegExp = /^\s*\[(X|x| )?\]\s*(.*)/gm;
+	const taskDecorationType = vscode.window.createTextEditorDecorationType({
+		after: {
+			margin: '0 0 0 0.25em',
+			color: 'gray',
+		},
+	})
 
-    let decorations: vscode.DecorationOptions[] = [];
-    let boldAndLargeDecorations: vscode.DecorationOptions[] = [];
+	const completedDecorationType =
+		vscode.window.createTextEditorDecorationType({
+			backgroundColor: completionHighlightsColor,
+		})
 
-    let taskMatch;
-    let numTasks = 0;
-    let overFlowSubTask = null;
-    let nextMatch = taskRegex.exec(text);
+	const incompleteDecorationType =
+		vscode.window.createTextEditorDecorationType({
+			backgroundColor: incompleteHighlightsColor,
+		})
 
-    while ((taskMatch = nextMatch) !== null) {
-      const startPos = editor.document.positionAt(taskMatch.index + 1); // Start after the opening bracket
-      const endPos = editor.document.positionAt(
-        taskMatch.index + taskMatch[0].length - 1 // End before the closing bracket
-      );
+	let disposable = vscode.commands.registerCommand(
+		'smarTODO.openSettings',
+		() => {
+			vscode.commands.executeCommand(
+				'workbench.action.openSettings',
+				'smarTODO'
+			)
+		}
+	)
 
-      nextMatch = taskRegex.exec(text);
-      let subtasks: SubTask[] = [];
+	context.subscriptions.push(disposable)
 
-      let taskEndIndex = nextMatch?.index ?? text.length;
+	let tasks: { [key: number]: Task } = {}
 
-      let subTaskMatch;
-      let nextSubTaskMatch: RegExpExecArray | null =
-        overFlowSubTask ?? subTaskRegex.exec(text);
+	let findAndUpdateTODOs = () => {
+		const editor = vscode.window.activeTextEditor
+		if (!editor) {
+			return
+		}
+		const document = editor.document
+		const text = document.getText()
+		const taskRegex: RegExp = /{([^}]+)}/g
+		const subTaskRegex: RegExp = /^\s*\[(X|x|-| )?\]\s*(.*)/gm
 
-      while (nextSubTaskMatch && nextSubTaskMatch.index <= taskEndIndex) {
-        overFlowSubTask !== null
-          ? (subTaskMatch = overFlowSubTask)
-          : (subTaskMatch = nextSubTaskMatch);
-        nextSubTaskMatch = subTaskRegex.exec(text);
-        nextSubTaskMatch !== null && nextSubTaskMatch.index > taskEndIndex
-          ? (overFlowSubTask = nextSubTaskMatch)
-          : (overFlowSubTask = null);
+		let taskDecorations: vscode.DecorationOptions[] = []
+		let completedSubtaskDecorations: vscode.DecorationOptions[] = []
+		let incompleteSubtaskDecorations: vscode.DecorationOptions[] = []
 
-        subtasks.push({
-          completed: subTaskMatch[1]?.toLowerCase() === "x" || false,
-          title: subTaskMatch[2].trim(),
-        });
-      }
+		let taskMatch
+		let numTasks = 0
+		let overFlowSubTask = null
+		let nextMatch = taskRegex.exec(text)
 
-      tasks[numTasks] = {
-        title: taskMatch[1].trim(),
-        completed: subtasks.filter((st) => st.completed).length,
-        total: subtasks.length,
-        subtasks: subtasks,
-        startPos: startPos,
-        endPos: endPos,
-      };
+		while ((taskMatch = nextMatch) !== null) {
+			const startPos = editor.document.positionAt(taskMatch.index) // Start after the opening bracket
+			const endPos = editor.document.positionAt(
+				taskMatch.index + taskMatch[0].length // End before the closing bracket
+			)
 
-      const decoration = {
-        range: new vscode.Range(startPos, endPos),
-        renderOptions: {
-          after: {
-            contentText: ` (${tasks[numTasks].completed}/${tasks[numTasks].total})`,
-          },
-        },
-      };
-      decorations.push(decoration);
+			nextMatch = taskRegex.exec(text)
+			let subtasks: SubTask[] = []
 
-      const boldAndLargeDecoration = {
-        range: new vscode.Range(startPos, endPos),
-      };
-      boldAndLargeDecorations.push(boldAndLargeDecoration);
+			let taskEndIndex = nextMatch?.index ?? text.length
 
-      numTasks++;
-    }
+			let subTaskMatch
+			let nextSubTaskMatch: RegExpExecArray | null =
+				overFlowSubTask ?? subTaskRegex.exec(text)
 
-    editor.setDecorations(taskDecorationType, decorations);
-    editor.setDecorations(boldAndLargeDecorationType, boldAndLargeDecorations);
-  };
+			while (nextSubTaskMatch && nextSubTaskMatch.index <= taskEndIndex) {
+				overFlowSubTask !== null
+					? (subTaskMatch = overFlowSubTask)
+					: (subTaskMatch = nextSubTaskMatch)
+				nextSubTaskMatch = subTaskRegex.exec(text)
+				nextSubTaskMatch !== null &&
+				nextSubTaskMatch.index > taskEndIndex
+					? (overFlowSubTask = nextSubTaskMatch)
+					: (overFlowSubTask = null)
 
-  // Completion provider for {}
-  const mainTaskCompletionProvider =
-    vscode.languages.registerCompletionItemProvider(
-      "todo",
-      {
-        provideCompletionItems(
-          document: vscode.TextDocument,
-          position: vscode.Position
-        ) {
-          const linePrefix = document
-            .lineAt(position)
-            .text.substring(0, position.character);
-          if (!linePrefix.endsWith("{")) {
-            return undefined;
-          }
+				const completionStatus = (() => {
+					switch (subTaskMatch[1] as string) {
+						case 'X':
+						case 'x':
+							return CompletionStatus.COMPLETED
+						case '-':
+							return CompletionStatus.INCOMPLETE
+						default:
+							return CompletionStatus.NOT_STARTED
+					}
+				})()
 
-          const completionItem = new vscode.CompletionItem(
-            "{}",
-            vscode.CompletionItemKind.Snippet
-          );
-          completionItem.insertText = new vscode.SnippetString("$1}");
-          completionItem.documentation = new vscode.MarkdownString(
-            "Insert a TODO task template"
-          );
+				subtasks.push({
+					completionStatus,
+					title: subTaskMatch[2].trim(),
+				})
 
-          return [completionItem];
-        },
-      },
-      "{"
-    );
+				const subTaskStartPos = editor.document.positionAt(
+					subTaskMatch.index
+				)
+				const subTaskEndPos = editor.document.positionAt(
+					subTaskMatch.index + subTaskMatch[0].length
+				)
 
-  // Completion provider for []
-  const subTaskCompletionProvider =
-    vscode.languages.registerCompletionItemProvider(
-      "todo",
-      {
-        provideCompletionItems(
-          document: vscode.TextDocument,
-          position: vscode.Position
-        ) {
-          const linePrefix = document
-            .lineAt(position)
-            .text.substring(0, position.character);
-          if (!linePrefix.endsWith("[") && !linePrefix.endsWith("[")) {
-            return undefined;
-          }
+				const subTaskDecoration: vscode.DecorationOptions = {
+					range: new vscode.Range(subTaskStartPos, subTaskEndPos),
+				}
 
-          const completionItem = new vscode.CompletionItem(
-            "[ ]",
-            vscode.CompletionItemKind.Snippet
-          );
-          completionItem.insertText = new vscode.SnippetString(" ] $1");
-          completionItem.documentation = new vscode.MarkdownString(
-            "Insert a subtask template"
-          );
+				if (completionStatus === CompletionStatus.COMPLETED) {
+					completedSubtaskDecorations.push(subTaskDecoration)
+				} else if (completionStatus === CompletionStatus.INCOMPLETE) {
+					incompleteSubtaskDecorations.push(subTaskDecoration)
+				}
+			}
 
-          return [completionItem];
-        },
-      },
-      "[",
-      " "
-    );
+			const creationDateKey = `task-${numTasks}-creationDate`
+			let creationDate: Date
+			const storedDate =
+				context.workspaceState.get<string>(creationDateKey)
+			if (storedDate) {
+				creationDate = new Date(storedDate)
+			} else {
+				creationDate = new Date()
+				context.workspaceState.update(
+					creationDateKey,
+					creationDate.toISOString()
+				)
+			}
 
-  context.subscriptions.push(
-    mainTaskCompletionProvider,
-    subTaskCompletionProvider
-  );
+			tasks[numTasks] = {
+				title: taskMatch[1].trim(),
+				completed: subtasks.filter(
+					(st) => st.completionStatus === CompletionStatus.COMPLETED
+				).length,
+				total: subtasks.length,
+				subtasks: subtasks,
+				startPos: startPos,
+				endPos: endPos,
+				creationDate: creationDate,
+			}
 
-  vscode.window.onDidChangeActiveTextEditor(
-    findAndUpdateTODOs,
-    null,
-    context.subscriptions
-  );
-  vscode.workspace.onDidChangeTextDocument(
-    findAndUpdateTODOs,
-    null,
-    context.subscriptions
-  );
+			const decoration = {
+				range: new vscode.Range(startPos, endPos),
+				renderOptions: {
+					after: {
+						contentText:
+							` (${tasks[numTasks].completed}/${tasks[numTasks].total})` +
+							createDateTimeString(
+								tasks[numTasks].creationDate,
+								showCreationDate,
+								showCreationTime
+							),
+					},
+				},
+			}
+			taskDecorations.push(decoration)
 
-  if (vscode.window.activeTextEditor) {
-    findAndUpdateTODOs();
-  }
+			numTasks++
+		}
+
+		editor.setDecorations(taskDecorationType, taskDecorations)
+		editor.setDecorations(
+			completedDecorationType,
+			completedSubtaskDecorations
+		)
+		editor.setDecorations(
+			incompleteDecorationType,
+			incompleteSubtaskDecorations
+		)
+	}
+
+	// Completion provider for {}
+	const mainTaskCompletionProvider =
+		vscode.languages.registerCompletionItemProvider(
+			'todo',
+			{
+				provideCompletionItems(
+					document: vscode.TextDocument,
+					position: vscode.Position
+				) {
+					const linePrefix = document
+						.lineAt(position)
+						.text.substring(0, position.character)
+					if (!linePrefix.endsWith('{')) {
+						return undefined
+					}
+
+					const completionItem = new vscode.CompletionItem(
+						'{}',
+						vscode.CompletionItemKind.Snippet
+					)
+					completionItem.insertText = new vscode.SnippetString('$1}')
+					completionItem.documentation = new vscode.MarkdownString(
+						'Insert a TODO task template'
+					)
+
+					return [completionItem]
+				},
+			},
+			'{'
+		)
+
+	// Completion provider for []
+	const subTaskCompletionProvider =
+		vscode.languages.registerCompletionItemProvider(
+			'todo',
+			{
+				provideCompletionItems(
+					document: vscode.TextDocument,
+					position: vscode.Position
+				) {
+					const linePrefix = document
+						.lineAt(position)
+						.text.substring(0, position.character)
+					if (
+						!linePrefix.endsWith('[') &&
+						!linePrefix.endsWith('[')
+					) {
+						return undefined
+					}
+
+					const completionItem = new vscode.CompletionItem(
+						'[ ]',
+						vscode.CompletionItemKind.Snippet
+					)
+					completionItem.insertText = new vscode.SnippetString(
+						' ] $1'
+					)
+					completionItem.documentation = new vscode.MarkdownString(
+						'Insert a subtask template'
+					)
+
+					return [completionItem]
+				},
+			},
+			'[',
+			' '
+		)
+
+	context.subscriptions.push(
+		mainTaskCompletionProvider,
+		subTaskCompletionProvider
+	)
+
+	const createDateTimeString = (
+		date: Date,
+		showDate: boolean | undefined,
+		showTime: boolean | undefined
+	) => {
+		if (!showDate && !showTime) {
+			return ''
+		}
+
+		let dateString = ' Created: '
+		if (showDate) {
+			dateString += date.toLocaleDateString()
+		}
+		if (showTime) {
+			dateString += `${
+				showDate ? ' at ' : ''
+			}${date.toLocaleTimeString()}`
+		}
+
+		return dateString
+	}
+
+	vscode.window.onDidChangeActiveTextEditor(
+		findAndUpdateTODOs,
+		null,
+		context.subscriptions
+	)
+	vscode.workspace.onDidChangeTextDocument(
+		findAndUpdateTODOs,
+		null,
+		context.subscriptions
+	)
+
+	if (vscode.window.activeTextEditor) {
+		findAndUpdateTODOs()
+	}
 }
 
 export function deactivate() {}
--- src/types/tasks.ts
+++ src/types/tasks.ts
@@ -1,16 +1,25 @@
-import * as vscode from "vscode";
+import * as vscode from 'vscode'
 
 export type Task = {
-  title: string;
-  completed: number;
-  total: number;
-  subtasks: SubTask[];
-  startPos: vscode.Position;
-  endPos: vscode.Position;
-  tasks?: Task[];
-};
+	title: string
+	completed: number
+	total: number
+	subtasks: SubTask[]
+	startPos: vscode.Position
+	endPos: vscode.Position
+	creationDate: Date
+	completionDate?: Date
+	tasks?: Task[]
+}
 
 export type SubTask = {
-  completed: boolean;
-  title: string;
-};
+	// completed: boolean
+	completionStatus: CompletionStatus
+	title: string
+}
+
+export enum CompletionStatus {
+	COMPLETED,
+	INCOMPLETE,
+	NOT_STARTED,
+}
